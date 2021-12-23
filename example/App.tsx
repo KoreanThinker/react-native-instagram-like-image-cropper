5729c4e1658d877c3752eaee2a4ffa351a022e32
hyun <coderhyun476@gmail.com>
2021-12-23 19:46:41 +0900

    Create example

--- example/App.tsx
+++ example/App.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import InstagramLikeImageCropper from '../src';
 
 const App = () => {
+  const [croppedImage, setCroppedImage] = useState<string | null>(null);
+
   return (
     <View>
-      <Text />
+      <GestureHandlerRootView>
+        <InstagramLikeImageCropper
+          onCropped={data => setCroppedImage(data.croppedUri)}
+          image={{
+            width: 1600,
+            height: 700,
+            uri: 'https://dummyimage.com/1600x700/b5b5b5/ffffff.png',
+          }}
+        />
+        <Text>Cropped image</Text>
+        {croppedImage && (
+          <Image source={{uri: croppedImage}} style={styles.image} />
+        )}
+      </GestureHandlerRootView>
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 300,
+    height: 300,
+  },
+});
